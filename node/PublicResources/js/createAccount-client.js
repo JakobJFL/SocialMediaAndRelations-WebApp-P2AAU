93efaa5831e4acb0c692fc41fe89d9bc42082587
Jakob Lykke <47578462+JakobJFL@users.noreply.github.com>
2021-04-21 14:42:59 +0200

    messageBox fixed - min and max date

--- node/PublicResources/js/createAccount-client.js
+++ node/PublicResources/js/createAccount-client.js
@@ -1,10 +1,25 @@
-document.getElementById("accInfo").addEventListener("submit", sendData); // kører sendData når klikkes på knappen eller når der er enter
+let maxDate = new Date();
+maxDate.setFullYear(maxDate.getFullYear() - 13);
+let dd = maxDate.getDate();
+let mm = maxDate.getMonth()+1; //January is 0!
+let yyyy = maxDate.getFullYear();
+if(dd<10)
+    dd = '0'+dd;
+if(mm<10)
+	mm = '0'+mm;
 
+maxDate = yyyy+'-'+mm+'-'+dd;
+document.getElementById("birthDate").setAttribute("max", maxDate);
+
+document.getElementById("accInfo").addEventListener("submit", sendData); // kører sendData når klikkes på knappen eller når der er enter
 function sendData() {
 	let firstName = document.getElementById("firstName").value; // skriv id af det input du vil bruge 
 	let lastName = document.getElementById("lastName").value;  
 	let inputEmail = document.getElementById("inputEmail").value; 
 	let password = document.getElementById("password").value; 
+
+	let birthDate = document.getElementById("birthDate").value; 
+	console.log(birthDate);
 
 	let jsonBody = {
 		fname: firstName,
