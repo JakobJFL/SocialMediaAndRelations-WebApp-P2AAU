83a42d02c3e0f506a4bba8e6c361db3ece024e58
Jakob Lykke <47578462+JakobJFL@users.noreply.github.com>
2021-04-21 12:59:37 +0200

    createAccount fix

--- node/PublicResources/js/createAccount-client.js
+++ node/PublicResources/js/createAccount-client.js
@@ -1,13 +1,10 @@
-document.getElementById("accInfo").addEventListener("submit", sendData); // kører sendData når klikkes på knappen, skriv id af det form eller btn 
+document.getElementById("accInfo").addEventListener("submit", sendData); // kører sendData når klikkes på knappen eller når der er enter
 
 function sendData() {
-	console.log("lmao");
-
-	let firstName = String(document.getElementById("firstName").value); // skriv id af det input du vil bruge 
-	let lastName = String(document.getElementById("lastName").value);  
-	let inputEmail = String(document.getElementById("inputEmail").value); 
-	let password = String(document.getElementById("password").value); 
-	//let study = String(document.getElementById("inputFieldsOfStudy").value); Studieretning, dette skal måske tilføjes til senere brug
+	let firstName = document.getElementById("firstName").value; // skriv id af det input du vil bruge 
+	let lastName = document.getElementById("lastName").value;  
+	let inputEmail = document.getElementById("inputEmail").value; 
+	let password = document.getElementById("password").value; 
 
 	let jsonBody = {
 		fname: firstName,
@@ -19,17 +16,46 @@ function sendData() {
 		intrest4: null,
 		psw: password
 
-	};
-  	fetch('https://sw2c2-19.p2datsw.cs.aau.dk/node0/makeUser', {
+	}; 
+	fetch('../makeUser', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify(jsonBody),
-    })
-    .then(response => response.json())
-    .then(jsonData => console.log(jsonData))
+	})
+	.then(response => {
+		console.log(response.status);
+		let errorField = document.getElementById("errorField");
+		if (response.status === 200) {
+			window.location.replace("https://sw2c2-19.p2datsw.cs.aau.dk/node0/");
+		}
+		else if (response.status === 400) {
+			errorField.innerHTML = "Input er forkert, check email adresse og undgå specialtegn";
+		}
+		else {
+			errorField.innerHTML = "Der er opstået en ukendt fejl";
+		}
+	})
 	.catch((error) => {
 		console.error('Error:', error);
 	});
+}
+
+function check(input) {
+	if (input.value != document.getElementById('password').value) {
+		input.setCustomValidity('Adgangskoderne skal være identiske');
+	} else {
+		// input is valid -- reset the error message
+		input.setCustomValidity('');
+	}
+}
+
+function checkNames(input) {
+	if (String(input.value).length > 15) {
+		input.setCustomValidity('Navn er for lang');
+	} else {
+		// input is valid -- reset the error message
+		input.setCustomValidity('');
+	}
 }
