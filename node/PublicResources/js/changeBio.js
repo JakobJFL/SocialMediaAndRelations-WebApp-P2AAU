cebf45fe421038db9035e93f5b5ca206211b1313
Jakob Lykke <47578462+JakobJFL@users.noreply.github.com>
2021-04-26 09:45:17 +0200

    tab fix

--- node/PublicResources/js/changeBio.js
+++ node/PublicResources/js/changeBio.js
@@ -3,11 +3,11 @@ const edit_button = document.getElementById("edit-button");
 const end_button = document.getElementById("end-editing");
 
 edit_button.addEventListener("click", function() {
-  paragraph.contentEditable = true;
-  paragraph.style.backgroundColor = "#999999";
-} );
+	paragraph.contentEditable = true;
+	paragraph.style.backgroundColor = "#999999";
+});
 
 end_button.addEventListener("click", function() {
-  paragraph.contentEditable = false;
-  paragraph.style.backgroundColor = "white";
-} )
+	paragraph.contentEditable = false;
+	paragraph.style.backgroundColor = "white";
+});
--- node/siteChat.js
+++ node/siteChat.js
@@ -6,8 +6,7 @@ const messageLengthToAddDummy = 40;
 function printChatPage(userID, fname, lname, parmsGroupID) {
 	console.log("ID: " + userID + " loged on - Group: " + parmsGroupID);
     let top = `<!DOCTYPE html><html lang="en">`;
-    let bottom = 
-    `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    let bottom = `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script type="text/javascript" src="/js/eventsourceLib/eventsource.min.js"></script>
     <script type="text/javascript" src="/js/main-client.js"></script>
     <script type="text/javascript" src="/js/countdown.js"></script>
@@ -27,20 +26,20 @@ function printChatPage(userID, fname, lname, parmsGroupID) {
 }
 
 function printHead() {
-return `<head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  
-            <link rel="stylesheet" href="css/messages.css">
-            <link rel="stylesheet" href="css/chatRoomMain.css">
-            <link rel="stylesheet" href="css/color.css">
+	return `<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-            <title>Social Media and Relations WebApp-P2AAU</title>
-        </head>
-        <body>`;
+			<link rel="stylesheet" href="css/messages.css">
+			<link rel="stylesheet" href="css/chatRoomMain.css">
+			<link rel="stylesheet" href="css/color.css">
+
+			<title>Social Media and Relations WebApp-P2AAU</title>
+		</head>
+		<body>`;
 }
 
 function printBody(userID, fname, lname, parmsGroupID) {
@@ -57,21 +56,21 @@ function printBody(userID, fname, lname, parmsGroupID) {
 
     function addCard(title, subtitle, active, cGroupID, addActive) {
         return `<ul class="nav flex-column">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" onclick="changeGroup(${cGroupID})">
-            <div class="card ${addActive}">
-              <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${subtitle}</h6>
-                <p class="card-active online">${active}</p>
-              </div>
-            </div>
-          </a>
-        </li>
-      </ul>`
+				<li class="nav-item">
+				<a class="nav-link" aria-current="page" onclick="changeGroup(${cGroupID})">
+					<div class="card ${addActive}">
+					<div class="card-body">
+						<h5 class="card-title">${title}</h5>
+						<h6 class="card-subtitle mb-2 text-muted">${subtitle}</h6>
+						<p class="card-active online">${active}</p>
+					</div>
+					</div>
+				</a>
+				</li>
+			</ul>`
     }
     function bottomChatFun() {
-      return `</div>
+    	return `</div>
                 </div>
                     <div class="message-sender">
                     <div class="container-fluid p-2 px-4 py-3 bg-white">
@@ -88,19 +87,18 @@ function printBody(userID, fname, lname, parmsGroupID) {
                 </div>
                 </main>`;
     }
-    let countdownCard = `
-    <ul class="nav flex-column">
-      <li class="nav-item">
-        <div class="nav-link" aria-current="page" href="#">
-          <div class="static">
-            <div class="card-body">
-              <h6 class="card-subtitle text-muted" id="countdown">Ny gruppe om: </h6>
-            </div>
-          </div>
-        </div>
-      </li>
-    </ul>`
-    let topCard = `  <div class="container-fluid"><div class="row"><nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse"><div class="position-sticky pt-3">`;
+    let countdownCard = `<ul class="nav flex-column">
+						<li class="nav-item">
+							<div class="nav-link" aria-current="page" href="#">
+							<div class="static">
+								<div class="card-body">
+								<h6 class="card-subtitle text-muted" id="countdown">Ny gruppe om: </h6>
+								</div>
+							</div>
+							</div>
+						</li>
+						</ul>`
+    let topCard = `<div class="container-fluid"><div class="row"><nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse"><div class="position-sticky pt-3">`;
     let bottomCard = `</div></nav></div></div>`
     
     let topChat = `<main><div class="chat-box p-2 px-4 py-5 my-4"><div id="allChat">`;
@@ -109,7 +107,6 @@ function printBody(userID, fname, lname, parmsGroupID) {
         let dummy = "";
         if (message.length <= messageLengthToAddDummy )
             dummy = `<div class="dummy-space-left"></div>`
-
         let resSender = `<div class="media sender-msg mb-3">
         <img src="pictures/WICKED.png" alt="user" width="50" class="rounded-circle">
         <div class="media-body py-2 ml-3">
@@ -147,11 +144,11 @@ function printBody(userID, fname, lname, parmsGroupID) {
       for (let i = 1; i <= 5; i++) {
         let key = "u"+i; 
         if (group[key] === fname && !isNameDel)
-          isNameDel = true;
+        	isNameDel = true;
         else {
-          names += group[key];
-          if (i !== 5) 
-            names += ", "
+        	names += group[key];
+        	if (i !== 5) 
+            	names += ", "
         }
       }
       return names;
@@ -160,40 +157,37 @@ function printBody(userID, fname, lname, parmsGroupID) {
 	let promise = new Promise((resolve,reject) => {
 		let cards = topCard+countdownCard;
 		let groupID = parmsGroupID;
-    let bottomChat = "";
+		let bottomChat = "";
 		getGroups(userID).then(groupsData => {
-      if (!groupsData) 
+			if (!groupsData) 
 				reject("promiseReject(getGroups)");
 			for (const group of groupsData) {
-        let cardTitle = makeCardTitle(group);
+				let cardTitle = makeCardTitle(group);
 				if (!groupID) 
-				  groupID = group.group_id;
-        bottomChat = bottomChatFun();
-				if (group.group_id == groupID) { //Type conversion - groupID is string
-          cards += addCard(cardTitle, "Ej hvor det flot", "Aktiv nu", group.group_id, "active");
-        }  
+					groupID = group.group_id;
+				bottomChat = bottomChatFun();
+				if (group.group_id == groupID) 
+					cards += addCard(cardTitle, "Ej hvor det flot", "Aktiv nu", group.group_id, "active");
 				else 
 					cards += addCard(cardTitle, "Ej hvor det flot", "Aktiv nu", group.group_id, "");
+				getChats(groupID).then(chatsData => {
+					if (!chatsData) 
+						reject("promiseReject(getChats)");
+					let chats = "";
+					for (const chat of chatsData) {
+						let DBdate = String(chat.TIMESTAMP).split(/[- :]/);
+						//let date = new Date(DBdate[2] + " " + DBdate[1] + " " + DBdate[3] + " " + DBdate[4] + ":" + DBdate[5]);
+						let dateFormatted = DBdate[2] + "/" + DBdate[1] + " " + DBdate[3] + " " + DBdate[4] + ":" + DBdate[5];
+						if (chat.user_ID === userID) 
+							chats += addChatReciever(chat.msg_content, dateFormatted); 
+						else 
+							chats += addChatSender(chat.msg_content, chat.fname + " " + chat.lname, dateFormatted);
+					}
+					let res = header+cards+bottomCard+topChat+chats+bottomChat;
+					resolve(res);
+				}).catch(err => console.error(err));
 			}
-			getChats(groupID).then(chatsData => {
-        if (!chatsData) 
-					reject("promiseReject(getChats)");
-				let chats = "";
-				for (const chat of chatsData) {
-					let DBdate = String(chat.TIMESTAMP).split(/[- :]/);
-					//let date = new Date(DBdate[2] + " " + DBdate[1] + " " + DBdate[3] + " " + DBdate[4] + ":" + DBdate[5]);
-					let dateFormatted = DBdate[2] + "/" + DBdate[1] + " " + DBdate[3] + " " + DBdate[4] + ":" + DBdate[5];
-					if (chat.user_ID === userID) 
-            chats += addChatReciever(chat.msg_content, dateFormatted); 
-					else 
-						chats += addChatSender(chat.msg_content, chat.fname + " " + chat.lname, dateFormatted);
-				}
-
-        let res = header+cards+bottomCard+topChat+chats+bottomChat;
-				resolve(res);
-			  }).catch(err => console.error(err));
-		  }).catch(err => console.error(err));
+		}).catch(err => console.error(err));
     });
-
     return promise;
 }
